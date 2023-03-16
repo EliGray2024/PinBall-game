af172a868469e9078f6c269715b3e7302ea068de
EliGray20 <115156773+EliGray20@users.noreply.github.com>
2023-03-16 07:57:42 -0400

    finished The barriers of the launcher

--- Map.py
+++ Map.py
@@ -5,6 +5,10 @@ class Map():
     def __init__ (self, space):
         self.image = pygame.image.load("images/Map.png")
         self.rect = self.image.get_rect()
+        Arc1x = 814
+        Arc1y = 120
+        Arc2x = 772
+        Arc2y = 158
         
         self.lineSegments =[ 
             #box
@@ -24,6 +28,21 @@ class Map():
             [(869, 121), (869, 935)],
             [(869, 935), (800, 935)],
             [(800, 935), (800, 156)],
+            #launcher Arc 1
+            [(55+Arc1x,0+Arc1y), (51+Arc1x, -10+Arc1y)],
+            [(51+Arc1x, -10+Arc1y), (47+Arc1x,-20+Arc1y)],
+            [(47+Arc1x,-20+Arc1y), (42+Arc1x,-30+Arc1y)],
+            [(42+Arc1x,-30+Arc1y), (30+Arc1x,-42+Arc1y)],
+            [(30+Arc1x,-42+Arc1y), (20+Arc1x,-47+Arc1y)],
+            [(20+Arc1x,-47+Arc1y), (10+Arc1x, -51+Arc1y)],
+            [(10+Arc1x, -51+Arc1y), (0+Arc1x,-55+Arc1y)],
+            #Launcher Arc 2
+            [(28+Arc2x,0+Arc2y), (27+Arc2x,-7+Arc2y)],
+            [(27+Arc2x,-7+Arc2y), (23+Arc2x, -16+Arc2y)],
+            [(23+Arc2x, -16+Arc2y), (16+Arc2x,-23+Arc2y)],
+            [(16+Arc2x,-23+Arc2y), (16+Arc2x, -23+Arc2y)],
+            [(16+Arc2x, -23+Arc2y), (7+Arc2x,-27+Arc2y)],
+            [(7+Arc2x,-27+Arc2y), (0+Arc2x,-28+Arc2y)],
         ]
         
         self.lines  = []
